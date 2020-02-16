4ed78ea32c96b5efe3060c66bb7f3359cb277c1f
Malak Tayeh <malaktayeh@outlook.com>
2020-02-16 13:55:23 -0500

    removed posts folder with markdown files
    The app is connected to ContentfulCMS. There is no need to keep
    local blog posts anymore.

--- src/pages/about.js
+++ src/pages/about.js
@@ -9,9 +9,9 @@ const AboutPage = () => {
             <Head title="About" />
             <div id="about">
                 <div>
-                    <h1>About me <span role="img" aria-labelledby="jsx-a11y/accessible-emoji">👩‍💻</span></h1>
-                    <p>I am a self-directed learner who enjoys the challenge of creating dynamic and beautiful web apps in my freetime.</p>
-                    <p></p>
+                    <h2>About me <span role="img" aria-labelledby="jsx-a11y/accessible-emoji">👩‍💻</span></h2>
+                    <p>I am a self-directed learner who enjoys the challenge of creating dynamic and beautiful web apps in my free-time.</p>
+
                     <h3>Resume</h3>
                     <p>For all the HR Managers out there, here is my <a href="https://api.swiftcv.com/public/resume-pdf?handle=tenshi" target="_#">resume</a>.</p>
                 </div>                
--- src/posts/i-love-gatsby.md
+++
@@ -1,6 +0,0 @@
----
-title: 'I love Gatsby!'
-date: 'June 1, 2019'
----
-
-# Why Gatsby is awesome
