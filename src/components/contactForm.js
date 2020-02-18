3ab895ff445410c458aa15cfa16faeacfea5880d
Malak Tayeh <malaktayeh@outlook.com>
2020-02-17 23:03:57 -0500

    added form validations and ccs positions
    used a combination of CCS Grid and Flexbox! :)

--- src/components/contactForm.js
+++ src/components/contactForm.js
@@ -3,34 +3,33 @@ import contactFormStyles from './styles/contactForm.module.scss'
 
 const ContactForm = () => {
     return (
-        <div id="contactForm">
+        <div id="contactForm" className={contactFormStyles.form} >
             <form name="contact" method="post" action="/success" data-netlify="true" data-netlify-honeypot="bot-field">
                 <input type="hidden" name="bot-field" />
                 <input type="hidden" name="form-name" value="contact" />
-                <div>
+                <div className={contactFormStyles.contactForm}>
                     <label htmlFor="name">First name:</label>
-                    <input type="text" name="first name" id="firstName" />
+                    <input type="text" name="first name" id="firstName" placeholder="Please enter your first name." required />
                 </div>
-                <div>
+                <div className={contactFormStyles.contactForm}>
                     <label htmlFor="name">Last name:</label>
-                    <input type="text" name="last name" id="lastName" />
+                    <input type="text" name="last name" id="lastName" placeholder="Your last name is also required." required />
                 </div>
-                <div>
-                    <label htmlFor="email">Email address:</label>
-                    <input type="text" name="email" id="email" />
+                <div className={contactFormStyles.contactForm} >  
+                    <label htmlFor="email">Email:</label>
+                    <input type="email" name="email" required placeholder="Your email address, please." />
                 </div>
-                <div>
-                    <label htmlFor="message">Your message:</label>
-                    <textarea name="message" id="formMessage" rows="6" />
+                <div className={contactFormStyles.contactFormMessage}>
+                    <label htmlFor="message">Message:</label>
+                    <textarea name="message" id="formMessage" rows="6" placeholder="What would you like to share with me? :)" required />
                 </div>
-                <ul className="actions">
-                    <li>
-                        <input type="submit" value="Send Message"/>
-                    </li>
-                    <li>
-                        <input type="reset" value="Clear" />
-                    </li>
-                </ul>
+                <br/>
+                <div className="actions" >
+                    <div className={contactFormStyles.alignButtons}>
+                        <button type="reset" value="Clear">Clear</button>
+                        <button type="submit" value="Send Message">Send</button>
+                    </div>
+                </div>
             </form>
         </div>
     )
