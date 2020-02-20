ac8aff47cab37da8c78bc83b7b4df3dc48a5f662
Malak Tayeh <malaktayeh@outlook.com>
2020-02-19 23:17:42 -0500

    throw confetti on success page!"
    "

--- src/pages/success.js
+++ src/pages/success.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Head from '../components/head';
 import Layout from '../components/layout';
+import Confetti from '../components/confetti'
 
 
 const SuccessPage = () => (
@@ -9,6 +10,7 @@ const SuccessPage = () => (
         <div id="success">
             <h3>Success! <span role="img" aria-labelledby="jsx-a11y/accessible-emoji">🎉</span></h3>
             <p>Thank you for contacting me! I will get back to you as soon as I can.</p>
+            <Confetti />
         </div>
   </Layout>
 );
